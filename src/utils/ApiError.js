03f433ea0bf268cb5f41005faf83a09f1ce3e090
Sarthak Singh <singhsarthak07072006@gmail.com>
2026-07-30 12:40:16 +0530

    Create classes for standard API response and API errors

---
+++ src/utils/ApiError.js
@@ -0,0 +1,17 @@
+class ApiError extends Error {
+    constructor(statusCode, message, data, stack="", errors=[]) {
+        super(message);
+        this.data = null;
+        this.statusCode = statusCode;
+        this.success = statusCode >= 400;
+        this.errors = errors;
+        this.success = false;
+
+        if (stack)
+            this.stack = stack;
+        else
+            this.stack = Error.captureStackTrace(this, this.constructor);
+    };
+};
+
+export { ApiError };
