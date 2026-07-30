0b1cfce231046815c6cb5991c72b6e661b1777ec
Sarthak Singh <singhsarthak07072006@gmail.com>
2026-07-30 11:40:28 +0530

    Setup project and express server

---
+++ src/app.js
@@ -0,0 +1,24 @@
+import express from "express"; 
+import cors from "cors";
+import "dotenv/config";
+
+const app = express();
+
+// basic configuration
+app.use(express.json({ limit: "256kb" }));
+app.use(express.urlencoded({ extended: true, limit: "256kb"  }));
+app.use(express.static("public"));
+
+// configure CORS
+app.use(cors({
+    origin: process.env.CORS_ORIGIN?.split(",") || "http://localhost:5173",
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+    allowedHeaders: ["Authorization", "Content-Type"]
+}));
+
+app.get("/health", (req, res) => {
+    res.sendStatus(200);
+});
+
+export { app };
