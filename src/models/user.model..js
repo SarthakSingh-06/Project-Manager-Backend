4e667573e6aa38fcae29ad8ddc9aa128962b3231
Sarthak Singh <singhsarthak07072006@gmail.com>
2026-07-30 23:55:50 +0530

    add user model with password hashing and email verification fields

---
+++ src/models/user.model..js
@@ -0,0 +1,74 @@
+import { Schema, model } from "mongoose";
+import bcrypt from "bcrypt";
+
+const userSchema = new Schema({
+    profileImage: {
+        type: {
+            url: String,
+            localPath: String
+        },
+        default: {
+            url: "https://placehold.co/200x200",
+            localPath: ""
+        }
+    },
+    username: {
+        type: String,
+        unique: true,
+        required: true,
+        lowercase: true,
+        trim: true,
+        index: true
+    },
+    email: {
+        type: String,
+        unique: true,
+        required: true,
+        lowercase: true,
+        trim: true
+    },
+    fullname: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    password: {
+        type: String,
+        required: [true, "Password is required."]
+    },
+    isEmailVerified: {
+        type: Boolean,
+        default: false
+    },
+    refreshToken: {
+        type: String
+    },
+    forgotPasswordToken: {
+        type: String
+    },
+    forgotPasswordTokenExpiry: {
+        type: Date
+    },
+    emailVerificationToken: {
+        type: String
+    },
+    emailVerificationTokenExpiry: {
+        type: Date
+    }
+}, {
+    timestamps: true
+});
+
+userSchema.pre("save", async function(next) {
+    if (!this.isModified("password")) next();
+
+    this.password = await bcrypt.hash(this.password, 12);
+    next();
+});
+
+userSchema.methods.isPasswordCorrect = async function(password) {
+    const result = await bcrypt.compare(password, this.password);
+    return result;
+};
+
+export const User = model("User", userSchema);
